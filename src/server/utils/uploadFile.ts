7d9eab2df21c2be61998168d6a4c807e89927d15
gustavocadev <gustavocadev@gmail.com>
2024-07-14 11:17:43 -0500

    Add upload file function

---
+++ src/server/utils/uploadFile.ts
@@ -0,0 +1,36 @@
+import {
+  v2 as cloudinary,
+  UploadApiOptions,
+  UploadApiResponse,
+} from 'cloudinary';
+
+export const uploadFile = async (file: File): Promise<string | null> => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  try {
+    const uploadOptions = { resource_type: 'auto' } satisfies UploadApiOptions;
+
+    const bytes = new Uint8Array(await file.arrayBuffer());
+
+    // upload through bytes
+    const result: UploadApiResponse | undefined = await new Promise(
+      (resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream(uploadOptions, (error, result) => {
+            if (error) return reject(error);
+            return resolve(result);
+          })
+          .end(bytes);
+      }
+    );
+    if (!result) throw new Error('Error uploading file');
+
+    return result.secure_url;
+  } catch (error) {
+    return null;
+  }
+};
