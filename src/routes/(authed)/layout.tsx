d47bc38dbde826385b3ca54a1dc5546ff2a833b7
gustavocadev <gustavocadev@gmail.com>
2024-07-14 11:19:13 -0500

    chore: Add SocketProvider and ProjectProvider context providers in layout.tsx

--- src/routes/(authed)/layout.tsx
+++ src/routes/(authed)/layout.tsx
@@ -3,6 +3,8 @@ import { routeLoader$ } from '@builder.io/qwik-city';
 import { Toaster } from 'qwik-sonner';
 import { CreateProjectModal } from '~/components/project/CreateProjectModal';
 import { Header } from '~/components/shared/Header';
+import { ProjectProvider } from '~/context/project/ProjectProvider';
+import { SocketProvider } from '~/context/socket/SocketProvider';
 import { handleRequest } from '~/server/db/lucia';
 import { findOneUser } from '~/server/services/user/user';
 
@@ -20,28 +22,20 @@ export const useUserDataLoader = routeLoader$(async (event) => {
   };
 });
 
-// export const useSignoutAction = routeAction$(async (values, event) => {
-//   const authRequest = auth.handleRequest(event);
-//   const { session } = await authRequest.validateUser();
-
-//   if (!session) throw event.redirect(303, '/');
-
-//   auth.invalidateSession(session.sessionId);
-//   authRequest.setSession(null);
-
-//   throw event.redirect(303, '/');
-// });
-
 export default component$(() => {
   return (
-    <div>
-      <Header />
-      <main class="flex-1 p-10">
-        <Slot />
-      </main>
+    <SocketProvider>
+      <ProjectProvider>
+        <div>
+          <Header />
+          <main class="flex-1 p-10">
+            <Slot />
+          </main>
 
-      <CreateProjectModal />
-      <Toaster />
-    </div>
+          <CreateProjectModal />
+          <Toaster />
+        </div>
+      </ProjectProvider>
+    </SocketProvider>
   );
 });
