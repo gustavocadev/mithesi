d36f30f336b21b0e3a7d3cfe66d026858aae3a48
gustavocadev <gustavocadev@gmail.com>
2024-07-14 13:54:22 -0500

    refactor: Update database connection string

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
 const pool = new pg.Pool({
-  connectionString: Bun.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema });
--- src/server/utils/uploadFile.ts
+++ src/server/utils/uploadFile.ts
@@ -7,9 +7,9 @@ import {
 // I don't must mix the server and client code otherwise I will have a lot of problems with navigation
 export const uploadFile = async (file: File): Promise<string | null> => {
   cloudinary.config({
-    cloud_name: Bun.env.CLOUDINARY_CLOUD_NAME,
-    api_key: Bun.env.CLOUDINARY_API_KEY,
-    api_secret: Bun.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
   try {
