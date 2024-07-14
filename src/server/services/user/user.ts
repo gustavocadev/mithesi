0a766566f3b495e51d54f5407db2fa7ac5f8113e
gustavocadev <gustavocadev@gmail.com>
2024-07-14 13:05:32 -0500

    refactor: Update findOneUser and findOneUserByEmail functions to return null if user is not found

--- src/server/services/user/user.ts
+++ src/server/services/user/user.ts
@@ -3,18 +3,26 @@ import { db } from '~/server/db/db';
 import { userTable } from '~/server/db/schema';
 import { User } from './entities/user';
 
-export const findOneUser = async (userId: string): Promise<User> => {
+export const findOneUser = async (userId: string): Promise<User | null> => {
   const [user] = await db
     .select()
     .from(userTable)
     .where(eq(userTable.id, userId));
+
+  if (!user) return null;
+
   return user;
 };
 
-export const findOneUserByEmail = async (userEmail: string): Promise<User> => {
+export const findOneUserByEmail = async (
+  userEmail: string
+): Promise<User | null> => {
   const [user] = await db
     .select()
     .from(userTable)
     .where(eq(userTable.email, userEmail));
+
+  if (!user) return null;
+
   return user;
 };
