939026d575c9bf8fa70ac0fa8ab063b2c4f8da20
gustavocadev <gustavocadev@gmail.com>
2024-07-14 13:49:37 -0500

    refactor: Update database connection string to use Bun.env instead of process.env

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: Bun.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema });
