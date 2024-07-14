26efd4a0507e47c9cad7a4f3f4df6f5fa3f22b2b
gustavocadev <gustavocadev@gmail.com>
2024-07-13 22:03:59 -0500

    chore: Update redirect paths in authed route

--- src/routes/(authed)/index.tsx
+++ src/routes/(authed)/index.tsx
@@ -4,7 +4,7 @@ import { handleRequest } from '~/server/db/lucia';
 export const onGet: RequestHandler = async ({ redirect, cookie }) => {
   const authRequest = handleRequest({ cookie });
   const { session } = await authRequest.validateUser();
-  if (!session) throw redirect(303, '/login');
+  if (!session) throw redirect(303, '/login/');
 
-  throw redirect(303, '/projects');
+  throw redirect(303, '/projects/');
 };
