b409f9777ed3f167da9a185e3cb89a143c41a8b0
gustavocadev <gustavocadev@gmail.com>
2024-07-14 19:23:03 -0500

    refactor: Update ProjectPost component to include like functionality

--- src/components/project/ProjectPost.tsx
+++ src/components/project/ProjectPost.tsx
@@ -2,10 +2,50 @@ import { component$, type PropsOf } from '@builder.io/qwik';
 import { Card } from '../ui/card/card';
 import { Badge } from '~/components/ui/badge/badge';
 import { Button } from '~/components/ui/button/button';
-import { LuMessageCircle, LuThumbsUp, LuShare2 } from '@qwikest/icons/lucide';
-import type { SelectProject } from '~/server/db/schema';
+import { LuMessageCircle, LuShare2 } from '@qwikest/icons/lucide';
+import { TbHeart, TbHeartFilled } from '@qwikest/icons/tablericons';
+
+import { type SelectProject } from '~/server/db/schema';
 import { formatDate } from '~/utils/formatDate';
 import { cn } from '@qwik-ui/utils';
+import {
+  Form,
+  globalAction$,
+  useNavigate,
+  z,
+  zod$,
+} from '@builder.io/qwik-city';
+import {
+  createUserLike,
+  deleteUserLike,
+} from '~/server/services/user-like/user-like';
+import { handleRequest } from '~/server/db/lucia';
+
+export const useLikeProjectAction = globalAction$(
+  async (values, { cookie, redirect }) => {
+    const authRequest = handleRequest({ cookie });
+    const { user } = await authRequest.validateUser();
+    if (!user) throw redirect(303, '/login');
+
+    if (values.intent === 'createLike') {
+      await createUserLike({
+        projectId: values.id,
+        userId: user.id,
+      });
+    }
+
+    if (values.intent === 'deleteLike') {
+      await deleteUserLike(values.id);
+    }
+
+    return {};
+  },
+
+  zod$({
+    intent: z.enum(['createLike', 'deleteLike']),
+    id: z.string(),
+  })
+);
 
 export type ProjectPostProps = {
   id: string;
@@ -16,10 +56,13 @@ export type ProjectPostProps = {
   urlImg?: string | null;
   projectStatus: SelectProject['status'];
   authorName: string;
+  userLikeId?: string | null;
+  likes: number;
 } & PropsOf<'div'>;
 
 export const ProjectPost = component$<ProjectPostProps>(
   ({
+    id,
     title,
     description,
     urlPdf,
@@ -27,8 +70,13 @@ export const ProjectPost = component$<ProjectPostProps>(
     projectStatus,
     urlImg,
     authorName,
+    userLikeId,
+    likes,
     ...props
   }) => {
+    const likeProjectAction = useLikeProjectAction();
+    const nav = useNavigate();
+
     return (
       <Card.Root
         class={cn(
@@ -36,6 +84,7 @@ export const ProjectPost = component$<ProjectPostProps>(
           props.class
         )}
         {...props}
+        onClick$={() => nav('/projects/' + id + '/')}
       >
         <Card.Header>
           <div class="flex items-start justify-between gap-4">
@@ -47,6 +96,9 @@ export const ProjectPost = component$<ProjectPostProps>(
               href={urlPdf}
               target="_blank"
               class="text-primary hover:underline flex-shrink-0"
+              onClick$={(e) => {
+                e.stopPropagation();
+              }}
             >
               Ver PDF
             </a>
@@ -89,18 +141,43 @@ export const ProjectPost = component$<ProjectPostProps>(
           </div>
           <div class="mt-4 flex items-center justify-between">
             <div class="flex items-center gap-2">
+              <Form action={likeProjectAction}>
+                <input type="hidden" name="id" value={id} />
+                <input
+                  type="hidden"
+                  name="intent"
+                  value={userLikeId ? 'deleteLike' : 'createLike'}
+                />
+                <Button
+                  look="ghost"
+                  size="icon"
+                  class="rounded-full hover:bg-muted gap-1 w-full px-4"
+                  type="submit"
+                  onClick$={(e) => {
+                    // to prevent redirecting to the project page
+                    e.stopPropagation();
+                  }}
+                >
+                  {userLikeId ? (
+                    <TbHeartFilled color="red" font-size={22} />
+                  ) : (
+                    <TbHeart font-size={22} />
+                  )}
+                  <span style={{ color: userLikeId ? 'red' : '' }}>
+                    {likes}
+                  </span>
+                  <span class="sr-only">Like</span>
+                </Button>
+              </Form>
               <Button
                 look="ghost"
                 size="icon"
                 class="rounded-full hover:bg-muted"
-              >
-                <LuThumbsUp class="w-5 h-5" />
-                <span class="sr-only">Like</span>
-              </Button>
-              <Button
-                look="ghost"
-                size="icon"
-                class="rounded-full hover:bg-muted"
+                onClick$={(e) => {
+                  // to prevent redirecting to the project page
+                  e.stopPropagation();
+                  alert('Feature not implemented yet');
+                }}
               >
                 <LuMessageCircle class="w-5 h-5" />
                 <span class="sr-only">Comment</span>
@@ -109,6 +186,11 @@ export const ProjectPost = component$<ProjectPostProps>(
                 look="ghost"
                 size="icon"
                 class="rounded-full hover:bg-muted"
+                onClick$={(e) => {
+                  // to prevent redirecting to the project page
+                  e.stopPropagation();
+                  alert('Feature not implemented yet');
+                }}
               >
                 <LuShare2 class="w-5 h-5" />
                 <span class="sr-only">Share</span>
