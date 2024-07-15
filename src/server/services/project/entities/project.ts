fff410e94a698773fd57d0520f42a3a14b8b1202
gustavocadev <gustavocadev@gmail.com>
2024-07-14 19:19:49 -0500

    refactor: Add userLike count to Project entity

--- src/server/services/project/entities/project.ts
+++ src/server/services/project/entities/project.ts
@@ -1,5 +1,7 @@
-import { SelectProject, SelectUser } from '~/server/db/schema';
+import { SelectProject, SelectUser, SelectUserLike } from '~/server/db/schema';
 
 export type Project = SelectProject & {
   user: SelectUser;
+  userLike?: SelectUserLike | null;
+  likes: number;
 };
