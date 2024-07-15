ef521180f3c9e02b62eb180682889ea10d58f044
gustavocadev <gustavocadev@gmail.com>
2024-07-14 19:20:09 -0500

    refactor: Update findOneThesisProject function to include userLike count and likes

--- src/server/services/project/project.ts
+++ src/server/services/project/project.ts
@@ -1,6 +1,6 @@
-import { eq } from 'drizzle-orm';
+import { count, eq } from 'drizzle-orm';
 import { db } from '~/server/db/db';
-import { thesisProject, userTable } from '~/server/db/schema';
+import { thesisProject, userLike, userTable } from '~/server/db/schema';
 import type { CreateProjectDto } from './dto/project';
 import { Project } from './entities/project';
 
@@ -11,14 +11,20 @@ export const findOneThesisProject = async (
     .select({
       project: thesisProject,
       user: userTable,
+      userLike: userLike,
+      likes: count(userLike.id),
     })
     .from(thesisProject)
     .innerJoin(userTable, eq(userTable.id, thesisProject.userId))
-    .where(eq(thesisProject.id, projectId));
+    .leftJoin(userLike, eq(userLike.thesisProjectId, thesisProject.id))
+    .where(eq(thesisProject.id, projectId))
+    .groupBy(userTable.id, thesisProject.id, userLike.id);
 
   return {
     ...projectFound.project,
     user: projectFound.user,
+    userLike: projectFound.userLike,
+    likes: projectFound.likes,
   };
 };
 
@@ -29,15 +35,21 @@ export const findProjectsByUserId = async (
     .select({
       projects: thesisProject,
       user: userTable,
+      userLike: userLike,
+      likes: count(userLike.id),
     })
     .from(thesisProject)
     .innerJoin(userTable, eq(userTable.id, thesisProject.userId))
-    .where(eq(thesisProject.userId, userId));
+    .leftJoin(userLike, eq(userLike.thesisProjectId, thesisProject.id))
+    .where(eq(thesisProject.userId, userId))
+    .groupBy(userTable.id, thesisProject.id, userLike.id);
 
   const projects = projectsByUser.map((project) => {
     return {
       ...project.projects,
       user: project.user,
+      userLike: project.userLike,
+      likes: project.likes,
     };
   });
 
