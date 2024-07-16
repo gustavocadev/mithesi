ea42966c6c8dd7b4ab9963d73ddb62706bbdd354
gustavocadev <gustavocadev@gmail.com>
2024-07-15 19:29:07 -0500

    refactor: Update Sidebar component to use useUserAuth hook for user authentication

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,15 +1,15 @@
 import { component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
-import { useUserDataLoader } from '~/routes/(authed)/layout';
 import { Button } from '../ui/button/button';
+import { useUserAuth } from '~/routes/(authed)/layout';
 
 type Props = {};
 
 export const Sidebar = component$<Props>(() => {
-  const userDataLoader = useUserDataLoader();
+  const user = useUserAuth();
   return (
     <aside class="md:w-80 lg:w-96 px-5 py-10 space-y-2">
-      <p class="text-xl font-bold">Hola: {userDataLoader.value.user?.name}</p>
+      <p class="text-xl font-bold">Hola: {user.value.name}</p>
 
       <Link href="/projects/new-project" class="block">
         <Button class="w-full text-md uppercase font-bold">
