63db240936099ea6949d38e334af0826ad5ec309
gustavocadev <gustavocadev@gmail.com>
2024-07-15 20:04:43 -0500

    Add more types to ProjectState type

--- src/context/project/ProjectContext.tsx
+++ src/context/project/ProjectContext.tsx
@@ -1,7 +1,10 @@
 import { createContextId, type Signal } from '@builder.io/qwik';
+import type { Project } from '~/server/services/project/entities/project';
 
 type ProjectState = {
   showCreateProjectModal: Signal<boolean>;
+  projectSelected: Signal<Project | null>;
+  projects: Signal<Project[]>;
 };
 
 export const ProjectContext = createContextId<ProjectState>('project.context');
