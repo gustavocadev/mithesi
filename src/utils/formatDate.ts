10710f50d04c0e912a70c39dbf69e6a4314f76ce
gustavocadev <gustavocadev@gmail.com>
2024-07-16 10:39:06 -0500

    refactor: Add formatDateToTimePassed utility function for displaying time passed

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,16 @@
+import { formatDistance } from 'date-fns';
+import { es } from 'date-fns/locale';
+
 export const formatDate = (date: Date): string => {
   return Intl.DateTimeFormat('es-ES', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   }).format(date);
+};
+
+export const formatDateToTimePassed = (date: Date): string => {
+  //example: hace 2 horas atrás/ 2 dias / 1 mes / 1 año/ 1 minuto / 1 segundo
+  // format distance
+  return formatDistance(date, new Date(), { locale: es });
 };
