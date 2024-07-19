ec877561a03ea356b2de9c5dce472c65deab1535
gustavocadev <gustavocadev@gmail.com>
2024-07-19 14:18:25 -0500

    feat: remove one committee member

--- src/components/project/ComitteeMember.tsx
+++ src/components/project/ComitteeMember.tsx
@@ -2,11 +2,14 @@ import { component$ } from '@builder.io/qwik';
 import { Form, globalAction$, z, zod$ } from '@builder.io/qwik-city';
 import { Button } from '../ui/button/button';
 import type { UserComitteeMember } from '~/server/services/comittee-member/types/comittee-member';
+import type { User } from 'lucia';
+import { LuX } from '@qwikest/icons/lucide';
+import { removeOneCommitteeMember } from '~/server/services/comittee-member/comittee-member';
 
-export const useActionRemoveContributor = globalAction$(
-  async () => {
+export const useRemoveOneCommitteMemberAction = globalAction$(
+  async (values) => {
     // delete one contributor from the project contributors
-    // await removeOneContributor(values.projectId, values.userId);
+    await removeOneCommitteeMember(values.projectId, values.userId);
 
     return {
       success: true,
@@ -19,30 +22,28 @@ export const useActionRemoveContributor = globalAction$(
 );
 
 type Props = {
-  contributor: UserComitteeMember;
+  committeeMember: UserComitteeMember;
   projectId: string;
-  authorId: string;
-  userAuthId: string;
+  user: User;
 };
 
 export const ComitteeMember = component$(
-  ({ contributor, projectId, authorId, userAuthId }: Props) => {
-    const actionRemoveContributor = useActionRemoveContributor();
+  ({ committeeMember, projectId, user }: Props) => {
+    const removeOneCommitteeMemberAction = useRemoveOneCommitteMemberAction();
     return (
       <div class="border-b p-5 flex justify-between items-center bg-white rounded">
         <div>
-          <p>{contributor.name}</p>
-          <p class="text-sm text-gray-700">{contributor.email}</p>
+          <p>{committeeMember.name + ' ' + committeeMember.lastName}</p>
+          <p class="text-sm text-gray-700">{committeeMember.email}</p>
         </div>
 
-        {authorId === userAuthId && (
-          <Form action={actionRemoveContributor}>
+        {user.role === 'admin' && (
+          <Form action={removeOneCommitteeMemberAction}>
             <input type="hidden" name="projectId" value={projectId} />
+            <input type="hidden" name="userId" value={committeeMember.id} />
 
-            <input type="hidden" name="userId" value={contributor.id} />
-
-            <Button type="submit" look="destructive">
-              Eliminar
+            <Button type="submit" look="ghost">
+              <LuX class="size-5" />
             </Button>
           </Form>
         )}
