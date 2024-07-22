a2188c1e5622b4fb1eb155acc450d455709571cb
gustavocadev <gustavocadev@gmail.com>
2024-07-21 19:02:52 -0500

    refactor: Remove isVisible field from CreateProjectDto

--- src/server/services/project/dto/project.ts
+++ src/server/services/project/dto/project.ts
@@ -2,5 +2,5 @@ import { SelectProject } from '~/server/db/schema';
 
 export type CreateProjectDto = Omit<
   SelectProject,
-  'createdAt' | 'updatedAt' | 'id' | 'status'
+  'createdAt' | 'updatedAt' | 'id' | 'status' | 'isVisible'
 >;
